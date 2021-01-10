71cb29f770b31729885b411dbf6a5b2677914826
Le Minh Tri <minhtri0795@gmail.com>
2021-01-10 09:34:44 +0700

    update

--- popup.js
+++ popup.js
@@ -4,9 +4,9 @@ function myfunction(){
     let html = document.querySelector('.link-list')
     linkVid.map(function(obj){
         return render+= `
-        <li style="display:flex; align-items:center; margin:8px 0; border:1px soild black;">
+        <li>
             <img style = "max-width:200px; max-height:150px; margin-right:8px" src = "${obj.thumb}">
-            <span><a class="myLink"  type="video/mp4" href="https://${obj.video}" download target="_blank">Download</a></span>
+            <span><a class="myLink"  type="video/mp4" href="https://${obj.video}"target="_blank">Download</a></span>
         </li>`
         
     })
